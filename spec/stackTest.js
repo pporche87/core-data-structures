5cf3f7689dd659c00c25dea99ac532c8d0cf8a3d
Patrick <porchewellness@gmail.com>
2017-04-11 11:24:53 -0700

    finishes the stackTest.js and stack.js data structures

---
+++ spec/stackTest.js
@@ -0,0 +1,55 @@
+import chai, { expect } from 'chai'
+import chaiChange from 'chai-change'
+import Stack from '../src/stack'
+
+chai.use(chaiChange)
+
+describe('Stack', () => {
+  'use strict'
+
+  it('exists', () => {
+    expect(Stack).to.be.a('function')
+  })
+
+
+  context('myPush()', () => {
+    it('pushes an element to the top of the stack.', () => {
+      const myStack = new Stack()
+
+      expect( () => myStack.myPush('foo')).to.alter(() => myStack.top, { from: 0, to: 1 })
+    })
+  })
+
+  context('myPop()', () => {
+    it('returns the top element in the stack or null if the stack is empty.', () => {
+      const myStack = new Stack()
+
+      expect(myStack.myPop() ).to.equal(null)
+    })
+  })
+
+  context('peek()', () => {
+    it('returns the top element in the stack or null if the stack is empty.', () => {
+      const myStack = new Stack()
+
+      expect(myStack.peek() ).to.equal(null)
+    })
+  })
+
+  context('isEmpty()', () => {
+    it('returns true if the stack is empty or false if not.', () => {
+      const myStack = new Stack()
+
+      expect(myStack.isEmpty() ).to.equal(true || false)
+    })
+  })
+
+  context('length()', () => {
+    it('returns the number of elements in the stack.', () => {
+      const myStack = new Stack()
+
+      expect(myStack.myLength() ).to.equal(0)
+    })
+  })
+
+})
